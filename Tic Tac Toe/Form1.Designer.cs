5b9c60c7648af4354426c507c41f00cc1a3cbeab
Claire Azagra <claire.azagra@gmail.com>
2022-06-03 17:43:06 +0800

    forms and buttons

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Plum;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button1.Location = new System.Drawing.Point(242, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Plum;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button2.Location = new System.Drawing.Point(242, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Image = global::Tic_Tac_Toe.Properties.Resources.tic_tac_toe_6_3_2022__2_;
+            this.label1.Location = new System.Drawing.Point(37, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(617, 210);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "                ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(681, 445);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tic Tac Toe/Form2.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.a1 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Plum;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button1.Location = new System.Drawing.Point(534, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "QUIT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.Color.Thistle;
+            this.a1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.a1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(170, 61);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(111, 102);
+            this.a1.TabIndex = 2;
+            this.a1.UseVisualStyleBackColor = false;
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.Color.Thistle;
+            this.c1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.c1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.Location = new System.Drawing.Point(170, 263);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(111, 102);
+            this.c1.TabIndex = 3;
+            this.c1.UseVisualStyleBackColor = false;
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.Thistle;
+            this.b1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(170, 162);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(111, 102);
+            this.b1.TabIndex = 4;
+            this.b1.UseVisualStyleBackColor = false;
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.Thistle;
+            this.b2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(280, 162);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(111, 102);
+            this.b2.TabIndex = 7;
+            this.b2.UseVisualStyleBackColor = false;
+            // 
+            // c2
+            // 
+            this.c2.BackColor = System.Drawing.Color.Thistle;
+            this.c2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.c2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.Location = new System.Drawing.Point(280, 263);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(111, 102);
+            this.c2.TabIndex = 6;
+            this.c2.UseVisualStyleBackColor = false;
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.Color.Thistle;
+            this.a2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.a2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.Location = new System.Drawing.Point(280, 61);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(111, 102);
+            this.a2.TabIndex = 5;
+            this.a2.UseVisualStyleBackColor = false;
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.Thistle;
+            this.b3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(390, 162);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(111, 102);
+            this.b3.TabIndex = 10;
+            this.b3.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Thistle;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(390, 263);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 102);
+            this.button9.TabIndex = 9;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.Color.Thistle;
+            this.a3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.a3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.Location = new System.Drawing.Point(390, 61);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(111, 102);
+            this.a3.TabIndex = 8;
+            this.a3.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button11.FlatAppearance.BorderSize = 0;
+            this.button11.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button11.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Plum;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.button11.Location = new System.Drawing.Point(44, 385);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(99, 35);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "NEW GAME";
+            this.button11.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.game_background_gd652e5681_1920;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(681, 445);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button button11;
+    }
+}
